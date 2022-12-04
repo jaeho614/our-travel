5ef89838509ae5871faa05f9605526db5851297a
jaeho614 <jaeho614a@gmail.com>
2022-12-04 20:04:06 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className="text-red-400">hello</div>;
+  return (
+    <div className="text-red-400">
+      hello<div>에러싫어</div>
+    </div>
+  );
 }
