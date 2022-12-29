55e5607bbd9f517ad1f863eab330728c04933b25
jaeho614 <jaeho614a@gmail.com>
2022-12-29 22:29:49 +0900

    12-29

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <div className="p-2 text-center border-b">홈</div>
-      <div className="grid grid-flow-row">
-        <div className="w-full h-12 bg-gray-500">
-          <button>시작하기</button>
+    <div className="flex flex-col h-screen">
+      <div className="h-10 p-2 text-center border-b">홈</div>
+      <div className="h-full grid grid-flow-row">
+        <div className="">
+          <img
+            className="h-full w-full"
+            alt="background"
+            src="https://images.unsplash.com/photo-1612977512598-3b8d6a498bbb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8JUVCJThDJTgwJUVEJTk1JTlDJUVCJUFGJUJDJUVBJUI1JUFEfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          />
         </div>
       </div>
       <div className="h-10 p-2 border-t border-gray-300">
